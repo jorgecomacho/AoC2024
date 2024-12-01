6942996e9dfa13f7974ec1496a0fa517c3b1faec
Jorgecomacho <john.kalin85@gmail.com>
2024-12-01 10:30:20 -0700

    Update day01.py
    cleanup

--- Day01/day01.py
+++ Day01/day01.py
@@ -10,31 +10,29 @@ def read_file(fname):
     lines = [i.strip() for i in f.readlines()]
     return lines
 
-def one(data):
-    total = 0
+def listify(data):
     left = []
     right = []
     for line in data:
         a, b = line.split()
         left.append(int(a))
         right.append(int(b))
+    return left, right
+
+def one(left, right):
+    total = 0
     for a, b in zip(sorted(left), sorted(right)):
         total += abs(a-b)
     return total
 
-def two(data):
+def two(left, right):
     total = 0
-    left = []
-    right = []
-    for line in data:
-        a, b = line.split()
-        left.append(int(a))
-        right.append(int(b))
     for a in left:
         total += a * right.count(a)
     return total
 
 if __name__ == "__main__":
     lines = read_file(filename)
-    print(one(lines))
-    print(two(lines))
+    left, right = listify(lines)
+    print(one(left, right))
+    print(two(left, right))
